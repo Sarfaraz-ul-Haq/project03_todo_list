31beae2a7e5bfdf6ab207b7c32c7572416f1788d
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-10 01:05:14 +0500

    add items

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todo_list: string[] = [];
+
+const selected = await inquirer.prompt({
+  name: "option",
+  type: "list",
+  choices: ["Add Items", "Delete Items", "View Items"],
+  message: "Select an option:",
+});
+
+if (selected.option == "Add Items") {
+  const userInput = await inquirer.prompt({
+    name: "item",
+    type: "input",
+    message: "Add your item",
+  });
+
+  todo_list.push(userInput.item);
+  console.log(todo_list);
+}
